2561ed15e43bd3de8f7101df8ef4c6de7de0fd8c
Yuichi Sugiyama <ozwald176@gmail.com>
2021-07-06 15:09:54 +0900

    update:210706

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import StartForm from '../components/StartForm'
+import { loadGetInitialProps } from 'next/dist/next-server/lib/utils';
 
 function StartPage() {
     return (
       <div>
         <div className="main_screen">
           <h1>ひらがなポーカー</h1>
+          {process.env.FIREBASE_API_KEY}
           <StartForm />
         </div>
       </div>
